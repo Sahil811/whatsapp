2f7e4e8c8359d41edb7dc248392dbca9981467aa
unknown <sahilsiddiqui.dev@gmail.com>
2020-09-07 16:07:44 +0530

    commit

---
+++ src/Chat.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Chat.css";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import { Avatar, IconButton } from "@material-ui/core";
+
+const Sidebar = () => {
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAVgnUCEIOKQ1B7NU-ZmThGvnvElCoZ9xM",
+  authDomain: "whatsapp-mern-aaeef.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-aaeef.firebaseio.com",
+  projectId: "whatsapp-mern-aaeef",
+  storageBucket: "whatsapp-mern-aaeef.appspot.com",
+  messagingSenderId: "862710054740",
+  appId: "1:862710054740:web:9e80c6b34bdd68565952c1",
+};
