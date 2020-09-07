81e485d103989b2be3952edecbe4f600f9807606
unknown <sahilsiddiqui.dev@gmail.com>
2020-09-07 17:20:57 +0530

    commit

--- src/Chat.js
+++ src/Chat.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./Chat.css";
 import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
 import { Avatar, IconButton } from "@material-ui/core";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
 const Sidebar = () => {
   return (
@@ -25,6 +27,31 @@ const Sidebar = () => {
             <MoreVert />
           </IconButton>
         </div>
+      </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Sahil</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message chat__reciever">
+          <span className="chat__name">Sahil</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input placeholder="Type a message" type="text" />
+          <button onClick={"sendMessage"} type="submit">
+            Send a Message
+          </button>
+        </form>
+        <MicIcon />
       </div>
     </div>
   );
